01c31fb5e279156fbd1dfc6f74dfbf9862b2fcba
Charles timothy henderson <35594263+navalboyblue@users.noreply.github.com>
2024-07-23 17:29:36 -0500

    Update hardhat.js

--- hardhat.js
+++ hardhat.js
@@ -2,6 +2,4 @@ sourcify: {
   enabled: true,
   apiUrl: "https://rpc.buildbear.io/verify/sourcify/server/intellectual-rogue-fdb73d90"
 }
-buildbear: {
-  url: "https://rpc.buildbear.io/intellectual-rogue-fdb73d90"
-}
+
